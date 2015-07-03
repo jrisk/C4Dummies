f7ca99cb9a1fb0fad06959ef4987fa17002a9c49
jrisk <joeyrsk@gmail.com>
2015-07-02 20:13:04 -0400

    C++ files

---
+++ C++/first.cc
@@ -0,0 +1,11 @@
+/* first C++ program */
+// line comment. prints sentences to screen
+// or whatever standard output is
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "I am a C++ program!";
+}
